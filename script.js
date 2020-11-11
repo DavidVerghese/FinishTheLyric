9cdec8062bd28ae8d9f5bb219764c2357cbf79bf
David Verghese <dj.verghese@gmail.com>
2020-11-10 20:57:18 -0500

    got the first line of dear prudence by the Beatles to display on the webpage

--- script.js
+++ script.js
@@ -3,14 +3,17 @@ const musicDiv = document.createElement('div')
 musicDiv.setAttribute('class', 'music')
 document.body.append(musicDiv)
 
-async function music(artist, song) {
+async function dearPrudence(artist, song) {
   //console.log('hi')
   const response = await axios.get(`https://api.lyrics.ovh/v1/${artist}/${song}`)
   try {
     const lyrics = response.data.lyrics
-    //console.log(response.data.lyrics)
+    let firstLine = ''
+    for (i = 0; i < 42; i++) {
+      firstLine += lyrics[i]
+    }
     const displayMusic = document.createElement('p')
-    displayMusic.innerText = response.data.lyrics
+    displayMusic.innerText = firstLine
     musicDiv.append(displayMusic)
   }
   catch (error) {
@@ -18,7 +21,7 @@ async function music(artist, song) {
   }
 }
 
-music('the beatles', 'something')
+music('beatles', 'dear prudence')
 
 // document.querySelector('#clicked').addEventListener("click", function () {
 //   music('the beatles', 'hey jude')
