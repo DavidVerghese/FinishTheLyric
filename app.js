efa0e74b444bfe37c3fb19fd355c37c03ea99328
David Verghese <dj.verghese@gmail.com>
2021-04-27 15:30:54 -0400

    Worked on styling

--- app.js
+++ app.js
@@ -367,7 +367,7 @@ async function finishTheLyric(artist, song, a, b, c, d, imageWidth, wrongAnswers
   }
 }
 
-startScreen()
+// startScreen();
 
 const questionOrderSorted = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
 const questionOrder = shuffle(questionOrderSorted)
